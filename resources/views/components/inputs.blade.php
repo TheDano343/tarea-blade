8583a70d353c4447ff0a01e8ee8f548fec0e5b3f
TheDano343 <mastergroovy@outlook.com>
2024-07-15 12:31:18 -0600

    Tarea realizada

---
+++ resources/views/components/inputs.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <label for="{{ $id }}">{{$nombre_label}}</label>
+    <div>
+        <input type="text" name="{{$nombreInput}}" id="{{$id}}">
+    </div>
+</div>
---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,6 @@
+<div>
+<label for="{{$nombreInput}}"></label>
+<div>
+    <textarea name="{{$nombreInput}}" id="{{$id}}" rows={{$numero_rows}}></textarea>
+</div>
+</div>
